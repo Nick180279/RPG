24c57a3da60d9d246ba188245100a025d36a1f50
Nikolay Panin <nickolay.panin@gmail.com>
2021-09-02 14:55:27 +0400

    Little refactoring

--- src/Person.java
+++ src/Person.java
@@ -7,18 +7,18 @@ public class Person {
     int Cha;
     int Intl;
     int Agl;
-    int Wis;
+    int Wil;
     Skill fight;
     int winContestCounter;
 
-    public Person (int str, int perc, int end, int cha, int intl, int agl, int wis, int fight) {
+    public Person (int str, int perc, int end, int cha, int intl, int agl, int wil, int fight) {
         this.Str = str;
         this.Perc = perc;
         this.End = end;
         this.Cha = cha;
         this.Intl = intl;
         this.Agl = agl;
-        this.Wis = wis;
+        this.Wil = wil;
         this.fight = new Skill(fight, "Swords");
         this.winContestCounter = 0;
     }
